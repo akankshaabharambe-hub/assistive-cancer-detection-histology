3f74941510c20cbabd4824338a4be4a00ee962c9
Akanksha Bharambe <akankshaabharambe@gmail.com>
2026-01-19 03:37:28 +0530

    add preprocessing package init and module docs

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,1 +1,6 @@
+"""
+Preprocessing utilities for histopathology screening.
 
+Includes patch extraction and lightweight background filtering
+to support large slide / high-resolution image workflows.
+"""
